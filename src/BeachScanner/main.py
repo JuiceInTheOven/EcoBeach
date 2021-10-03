47ab94453c5a9a806f11c9fa31115c78e24e99fe
Nikolai Damm <nikolaiemildamm@icloud.com>
2021-10-03 20:48:34 +0000

    Fixed query so it works

--- src/BeachScanner/main.py
+++ src/BeachScanner/main.py
@@ -1,34 +1,29 @@
 import folium
 import geopandas as gdp
 from pandas.core import api
-from sentinelsat import SentinelAPI
+from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
+from datetime import date
 import rasterio
 #import matplotlib.pyplot as plt
-#from rasterio import plot
-#from rasterio.plot import show
-#from rasterio.mask import geometry_mask
+from rasterio import plot
+from rasterio.plot import show
+from rasterio.mask import geometry_mask
 #from osgeo import gdal
 
 def main():
-    m = folium.Map([50.20, 11.4], zoom_start=11)
-    boundary = gdp.read_file("./map.geojson")
-    folium.GeoJson(boundary).add_to(m)
-    footprint = None
-    for i in boundary['geometry']:
-        footprint = i
-    
     user = "nikolai.damm"
     password = "fywfuP-qekfut-xomki3"
 
-    api = SentinelAPI(user, password, 'https://scihub.copernicus.eu/dhus')
+    api = SentinelAPI(user, password)
+    footprint = geojson_to_wkt(read_geojson('map.geojson'))
     products = api.query(footprint,
-                     date = ('20200109', '20200510'),
-                     platformname = 'Sentinel-2',
-                     processinglevel = 'Level-2A',
-                     cloudcoverpercentage = (0, 20))
-    gdf = api.to_geodataframe(products)
-    gdf_sorted = gdf.sort_values(['cloudcoverpercentage'], ascending=[True])
-    api.download(i);
+                    date = ('20200109', '20200510'),
+                    platformname = 'Sentinel-2',
+                    processinglevel = 'Level-2A',
+                    cloudcoverpercentage = (0, 20))
+    api.to_geodataframe(products).sort_values(['cloudcoverpercentage'], ascending=[True])
+    api.download_all(products)
+
     bands = r'...\GRANULE\L2A_T18TWL_A025934_20200609T155403\IMG_DATA\R10m'
     blue = rasterio.open(bands+'\T18TWL_20200609T154911_B02_10m.jp2') 
     green = rasterio.open(bands+'\T18TWL_20200609T154911_B03_10m.jp2') 
