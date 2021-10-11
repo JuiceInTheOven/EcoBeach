2c21f6b4ae50c86b0b393e61ef9f9e1b836c4f60
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-10-11 16:56:05 +0200

    Extracted methods in main.py

--- src/SentinelSatDownloader/main.py
+++ src/SentinelSatDownloader/main.py
@@ -1,6 +1,5 @@
 from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt, make_path_filter
-from flask import Flask, request
-import numpy as np
+from flask import Flask
 
 from flask_restful import reqparse
 
@@ -8,20 +7,34 @@ app = Flask(__name__)
 # http://localhost:105/persist?position=10 11&fromdate=YYYYMMDD&todate=YYYYMMDD
 @app.route('/download', methods=['GET', 'POST'])
 def download():
+    args = parseRequestArgs()
+    
+    makePositionArgArrayOfFloats(args)
+
+    user = "nikolai.damm"
+    password = "fywfuP-qekfut-xomki3"
+
+    rectangleQuery = createRectangleWtkQueryFromArgs(args)
+
+    api, products = querySentinelSatApi(args, user, password, rectangleQuery)
+
+    downloadProducts(api, products)
+    return
+
+def parseRequestArgs():
     parser = reqparse.RequestParser()
     parser.add_argument('position', type=str, help='a list of positions -> lat lon')
     parser.add_argument('fromdate', type=str, help="The earliest date to get map data from -> 'YYYYMMDD'")
     parser.add_argument('todate', type=str, help="The latest date to get map data from -> 'YYYYMMDD' or 'NOW' for current date")
 
     args = parser.parse_args()
-    
+    return args
+
+def makePositionArgArrayOfFloats(args):
     positions = args.position.split()
     args.position = [float(positions[0]), float(positions[1])]
 
-    #Download satelite imagery of beach locations to sentinelsat/downloads
-    user = "nikolai.damm"
-    password = "fywfuP-qekfut-xomki3"
-
+def createRectangleWtkQueryFromArgs(args):
     # Creates a small rectangle boundary box around a position, where the position is at the center of the rectangle.
     latRectSize = 0.005
     longRectSize = 0.0025
@@ -32,8 +45,9 @@ def download():
 
     # Creates a GeoJSON rectangle query in the well known text (wtk) format that queries the sentinel satellite for maps that contain our rectangle query.
     rectangleQuery = f"POLYGON (({topLeftCorner}, {bottomLeftCorner}, {bottomRightCorner}, {topRightCorner}, {topLeftCorner}))"
+    return rectangleQuery
 
-    # Queries the sentinel satellite with predefined parameters
+def querySentinelSatApi(args, user, password, rectangleQuery):
     api = SentinelAPI(user, password)
     623680114746094
     products = api.query(rectangleQuery,
@@ -41,13 +55,12 @@ def download():
                 processinglevel = 'Level-2A',
                 date = (args.fromdate, args.todate),
                 cloudcoverpercentage=(0, 20))
+                
+    return api,products
 
-    # Downloads selected data based on the query above
+def downloadProducts(api, products):
     nodefilter = make_path_filter("*/granule/*/img_data/r10m/*_tci_10m.jp2")
     api.download_all(products, "downloads", nodefilter=nodefilter)
-    return
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=105)
-
