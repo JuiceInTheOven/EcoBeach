51bf5e83885ebdadea4fb85f1c50b42dde167479
Nikolai Emil Damm <nikolaiemildamm@icloud.com>
2021-11-11 23:32:49 +0100

    test

--- src/NDWIAnalyzer/analyzer.py
+++ src/NDWIAnalyzer/analyzer.py
@@ -1,7 +1,14 @@
 from pyspark.sql import SparkSession
+# Start a SparkSession, and configure it like last time
+spark = SparkSession.builder.appName('streamTest') \
+    .config('spark.master','spark://spark-master:7077') \
+    .config('spark.executor.cores', 1) \
+    .config('spark.cores.max',1) \
+    .config('spark.executor.memory', '1g') \
+    .config('spark.sql.streaming.checkpointLocation','hdfs://namenode:9000/stream-checkpoint/') \
+    .getOrCreate()
 
 logFile = "README.md"  # Should be some file on your system
-spark = SparkSession.builder.master("spark://spark-master:7077").appName("SimpleApp").getOrCreate()
 logData = spark.read.text(logFile).cache()
 
 numAs = logData.filter(logData.value.contains('a')).count()
