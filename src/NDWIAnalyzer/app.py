8323186bf10f337c29fb434c1b1841b9ed0a1071
Nikolai Damm <nikolaiemildamm@icloud.com>
2021-11-14 12:26:49 +0000

    Began implementing NDWI analyzer logic

--- src/NDWIAnalyzer/app.py
+++ src/NDWIAnalyzer/app.py
@@ -1,36 +1,72 @@
 from pyspark.sql import SparkSession
-# Start a SparkSession, and configure it like last time
-spark = SparkSession.builder.appName('streamTest') \
-    .config('spark.master','spark://spark-master:7077') \
-    .config('spark.executor.cores', 1) \
-    .config('spark.cores.max',1) \
-    .config('spark.executor.memory', '2g') \
-    .config('spark.yarn.executor.memoryOverhead', 500) \
-    .config('spark.sql.streaming.checkpointLocation','hdfs://namenode:9000/stream-checkpoint/') \
-    .getOrCreate()
+from pyspark.sql.functions import explode, split, to_json, array, col
 
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
-print("I Work!!!!")
 
-# df = spark \
-#   .readStream \
-#   .format("kafka") \
-#   .option("kafka.bootstrap.servers", "host1:port1,host2:port2") \
-#   .option("subscribe", "topic1") \
-#   .load()
-# df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+def main():
+    spark = setUpSparkSession()
+    dataFrame = loadKafkaTopicStream(spark)
+    # dataFrame = analyzeNdwiImages(dataFrame)
+    # writeKafkaTopic(dataFrame)
+    spark.stop()
 
-# df.selectExpr("CAST(value AS STRING)").writeStream \
-#     .format('kafka') \
-#     .option("kafka.bootstrap.servers", "kafka:9092") \
-#     .option("topic", "sentences") \
-#     .start().awaitTermination()
 
-#spark.stop()
+def setUpSparkSession():
+    return SparkSession.builder.appName("ndwi-analyzer") \
+        .config('spark.master', 'spark://spark-master:7077') \
+        .config('spark.executor.cores', 1) \
+        .config('spark.cores.max', 1) \
+        .config('spark.executor.memory', '1g') \
+        .config('spark.sql.streaming.checkpointLocation', 'hdfs://namenode:9000/stream-checkpoint/') \
+        .getOrCreate()
+
+
+def loadKafkaTopicStream(spark):
+    return spark \
+        .readStream \
+        .format("kafka") \
+        .option("kafka.bootstrap.servers", "helsinki.faurskov.dev:9093, falkenstein.faurskov.dev:9095, nuremberg.faurskov.dev:9097") \
+        .option("subscribe", "ndwi_images") \
+        .load()
+
+
+def analyzeNdwiImages(dataFrame):
+    data = dataFrame.selectExpr(
+        "CAST(value AS STRING)"
+    )
+    print(data)
+    data = dataFrame.selectExpr(
+        "CAST(countryCode AS STRING)",
+        "CAST(locationName AS STRING)",
+        "CAST(geoPosition AS STRING)",
+        "CAST(date AS STRING)",
+        "CAST(imageName AS STRING)",
+        "CAST(image_bytes AS STRING"
+    )
+    print(data)
+    #resultSchema = createResultSchema()
+    #resultDataFrame
+    #mergedColumns = filteredWordCounts.withColumn('value', array(columns))
+    #return
+
+
+def createResultSchema():
+    return [col('countryCode'),
+            col('locationName'),
+            col('geoPosition'),
+            col('date'),
+            col('land_squareMeters'),
+            col('land_percentages'),
+            col('water_squareMeters'),
+            col('water_percentages')]
+
+
+def writeKafkaTopic(dataFrame):
+    dataFrame.selectExpr("CAST(value AS STRING)").writeStream \
+        .format('kafka') \
+        .option("kafka.bootstrap.servers", "kafka:9092") \
+        .option("topic", "sentences") \
+        .start().awaitTermination()
+
+
+if __name__ == '__main__':
+    main()
