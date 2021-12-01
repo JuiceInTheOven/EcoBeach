796ba9a28c7166e0f6d5ea053f638e6e16de08c6
Guli <gulacsi.gab@gmail.com>
2021-12-01 14:58:40 +0100

    #buildfix Upload missing files

---
+++ src/WebApi.DataAccess/Context/BeachDatabaseSettings.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApi.DataAccess.Context
+{
+
+    public class BeachDatabaseSettings : IBeachDatabaseSettings
+    {
+        public string BeachCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+
+    public interface IBeachDatabaseSettings
+    {
+        string BeachCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
+    }
+
+}
