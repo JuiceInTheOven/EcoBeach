5ca1d63d2a70ebae7f76150a7dbaef1e68e96769
Zsófia Bardócz <bardocz.zsofi@gmail.com>
2021-12-08 18:35:13 +0100

    Added internet and gps checker

--- src/AndroidProject/app/src/main/java/com/example/androidproject/activities/MainActivity.kt
+++ src/AndroidProject/app/src/main/java/com/example/androidproject/activities/MainActivity.kt
@@ -11,14 +11,21 @@ import com.example.androidproject.R
 import com.example.androidproject.databinding.ActivityMainBinding
 import com.example.androidproject.models.Beach
 import com.example.androidproject.models.CustomMarker
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 import android.content.Intent
+import android.location.LocationManager
+import android.content.DialogInterface
+import android.provider.Settings
+import android.app.AlertDialog.Builder
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.os.Build
+import com.google.android.gms.location.*
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -35,8 +42,92 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        getLastKnownLocation()
+        checkGPS()
+        checkInternet(this)
+    }
+
+    private fun checkInternet(context: Context){
+        if(checkForInternet(context)){
+            //Toast.makeText(this, "Internet is Enabled in your device", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            val builder = Builder(this)
+            builder
+                .setTitle("There's no Internet connection")
+                .setMessage("For a better experience, turn on Internet connection. If you pick OK, the application will be closed. " +
+                        "Please restart the app after you have turned on the internet.")
+                .setPositiveButton("OK") { _, _ -> finish() }
+                .setNegativeButton("No thanks") { _, _ ->  }
+            // Create the AlertDialog object and return it
+            builder.create().show()
+        }
+    }
+
+    private fun checkForInternet(context: Context): Boolean {
+
+        // register activity with the connectivity manager service
+        val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+
+        // if the android version is equal to M
+        // or greater we need to use the
+        // NetworkCapabilities to check what type of
+        // network has the internet connection
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // Returns a Network object corresponding to
+            // the currently active default data network.
+            val network = connectivityManager.activeNetwork ?: return false
+
+            // Representation of the capabilities of an active network.
+            val activeNetwork = connectivityManager.getNetworkCapabilities(network) ?: return false
+
+            return when {
+                // Indicates this network uses a Wi-Fi transport,
+                // or WiFi has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) -> true
+
+                // Indicates this network uses a Cellular transport. or
+                // Cellular has network connectivity
+                activeNetwork.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR) -> true
+
+                // else return false
+                else -> false
+            }
+        } else {
+            // if the android version is below M
+            @Suppress("DEPRECATION") val networkInfo =
+                connectivityManager.activeNetworkInfo ?: return false
+            @Suppress("DEPRECATION")
+            return networkInfo.isConnected
+        }
+    }
+
+    private fun checkGPS(){
+        val mLocationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        // Checking GPS is enabled
+        val isGPSEnabled = mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
+        if(isGPSEnabled){
+            //Toast.makeText(this, "GPS is Enabled in your device", Toast.LENGTH_SHORT).show()
+            fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+            getLastKnownLocation()
+        }
+        else{
+
+            //Toast.makeText(this, "GPS is not Enabled in your devide", Toast.LENGTH_SHORT).show()
+            val builder = Builder(this)
+            builder
+                .setTitle("GPS is not enabled")
+                .setMessage("For a better experience, turn on device location")
+                .setPositiveButton("OK") { _, _ ->
+                    val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
+                    startActivity(intent)
+                    finish()
+                }
+                .setNegativeButton("No thanks") { _, _ -> finish() }
+            // Create the AlertDialog object and return it
+            builder.create().show()
+        }
     }
 
     /**
@@ -72,18 +163,24 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
     }
 
-    private fun addBeachesToMap(){
+    private fun addBeachesToMap() {
         // Add a marker to the user's location
-        mMap.addMarker(MarkerOptions().position(userLocation).title("You are here :)").snippet(",${userLocation.latitude},${userLocation.longitude},"))
+        mMap.addMarker(
+            MarkerOptions().position(userLocation).title("You are here :)")
+                .snippet(",${userLocation.latitude},${userLocation.longitude},")
+        )
 
         val beaches = mutableListOf<Beach>()
         beaches.add(Beach("Kerteminde Nordstrand", 55.4578631605276, 10.66580564769334, 0.0))
         beaches.add(Beach("Hasmark Strand", 55.559144842780235, 10.466831402854728, 0.0))
         beaches.add(Beach("Nordenhuse Strand", 55.37738690518388, 10.771233901499762, 0.0))
 
-        beaches.forEach{ beach ->
-            mMap.addMarker(MarkerOptions().position(LatLng(beach.Lat, beach.Lng)).title(beach.Name).icon(
-                BitmapDescriptorFactory.defaultMarker(green)).snippet(beach.toString()))
+        beaches.forEach { beach ->
+            mMap.addMarker(
+                MarkerOptions().position(LatLng(beach.Lat, beach.Lng)).title(beach.Name).icon(
+                    BitmapDescriptorFactory.defaultMarker(green)
+                ).snippet(beach.toString())
+            )
         }
     }
 
@@ -130,9 +227,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
                             Manifest.permission.ACCESS_FINE_LOCATION
                         ) === PackageManager.PERMISSION_GRANTED)) {
                         Toast.makeText(this, "Permission Granted", Toast.LENGTH_SHORT).show()
+                        startActivity(this.intent)
                     }
                 } else {
                     Toast.makeText(this, "Permission Denied", Toast.LENGTH_SHORT).show()
+                    finish()
                 }
                 return
             }
